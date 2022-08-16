8ad7380c6820e2e2bf284628e9d0795da92b0957
Sergey.Litvinchuk <Sergey.Litvinchuk@innostage-group.ru>
2022-08-16 11:56:20 +0300

    Solve 04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,43 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const App = defineComponent({
+  // Имя позволяет программно получать имя компонента.
+  // Используется в devtools,
+  name: 'App',
+
+  data() {
+      return {
+          selectedMeetupId:undefined,
+          meetupTitle:undefined
+      };
+  },
+
+  watch: {
+    selectedMeetupId: {
+      handler() {
+        if(isNaN(this.selectedMeetupId)){
+          this.meetupTitle = '';
+          return;
+        }
+
+        if(this.selectedMeetupId < 1) this.selectedMeetupId = 1;
+        else if(this.selectedMeetupId > 5) this.selectedMeetupId = 5;
+
+        fetchMeetupById(this.selectedMeetupId).then((meetup) => this.meetupTitle = meetup.title);
+      },
+    },
+  },
+
+  // Как только приложение будет смонтировано - обновляем данные
+  mounted() {
+    this.selectedMeetupId = 1;
+  },
+});
+
+// Создаём новое приложение по корневому компоненту
+const app = createApp(App);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
