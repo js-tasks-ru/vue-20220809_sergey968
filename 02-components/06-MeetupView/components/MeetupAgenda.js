68dfb4ef939e64c7120a1345fe44f138d2d3199f
Sergey Litvinchuk <s19137@gmail.com>
2022-08-22 23:28:54 +0300

    Solve 02-components

---
+++ 02-components/06-MeetupView/components/MeetupAgenda.js
@@ -0,0 +1,25 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+
+export default defineComponent({
+  name: 'MeetupAgenda',
+
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  template: `
+    <ul class="agenda">
+      <li v-for="item in agenda" class="agenda__item">
+        <!-- meetup agenda item -->
+        <MeetupAgendaItem :agendaItem="item"/>
+      </li>
+    </ul>`,
+});
--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from './MeetupView.js';
+import { fetchMeetupById } from '../meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,46 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      meetup: null,
+      error: null,
+    };
+  },
+
+  watch: {
+    meetupId: {
+      immediate: true,
+      handler() {
+        this.meetup = null;
+        this.error = null;
+        fetchMeetupById(this.meetupId)
+          .then((meetup) => (this.meetup = meetup))
+          .catch((e) => (this.error = e));
+      },
+    },
   },
 
   template: `
     <div class="page-meetup">
       <!-- meetup view -->
-
-      <UiContainer>
+      <MeetupView v-if="meetup" :meetup = "meetup"/>
+      <UiContainer v-else-if="meetup === null && error === null">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
-
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-else>
+        <UiAlert v-if="error"> {{ error }} </UiAlert>
+        <UiAlert v-else> Unknown error </UiAlert>
       </UiContainer>
     </div>`,
 });
