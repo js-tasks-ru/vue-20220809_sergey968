d7d3f61404ed6c7ec3af834412b8d716661cb003
Sergey Litvinchuk <s19137@gmail.com>
2022-09-18 23:04:06 +0300

    Solve 06-wrappers/01-UiLink

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,14 @@
 <template>
-  <a class="link">Link!</a>
+  <a v-if="tag && tag == 'a'" class="link"><slot /></a>
+  <router-link v-else><slot /></router-link>
 </template>
 
 <script>
 export default {
   name: 'UiLink',
+  props: {
+    tag: { type: String, default: null },
+  },
 };
 </script>
 
